805aa513b20d4e3fdae452bd5bd4cdec7734dba0
Max Neuvians <max@neuvians.net>
2024-11-18 23:25:50 +0000

    debug: stash

--- valentine/lib/valentine_web/live/workspace_live/components/flash_component.ex
+++ valentine/lib/valentine_web/live/workspace_live/components/flash_component.ex
@@ -2,21 +2,13 @@ defmodule ValentineWeb.WorkspaceLive.Components.FlashComponent do
   use Phoenix.Component
   use PrimerLive
 
-  alias Phoenix.LiveView.JS
-
   def flash_group(assigns) do
     ~H"""
     <.alert_messages>
       <%= for {kind, message} <- @flash do %>
         <.alert state={kind} class="mt-2">
           <%= message %>
-          <.button
-            class="flash-close"
-            phx-click={
-              JS.push("lv:clear-flash", value: %{key: kind})
-              |> ValentineWeb.CoreComponents.hide("#flash-#{kind}")
-            }
-          >
+          <.button class="flash-close" phx-click="lv:clear-flash">
             <.octicon name="x-16" />
           </.button>
         </.alert>
