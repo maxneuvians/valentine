6f18057dc8f265b5b23f22b9c9cad697493d4656
Max Neuvians <max@neuvians.net>
2024-12-02 23:11:23 +0000

    fix: icons

--- valentine/lib/valentine_web/live/workspace_live/components/data_flow_component.ex
+++ valentine/lib/valentine_web/live/workspace_live/components/data_flow_component.ex
@@ -1,49 +1,62 @@
 defmodule ValentineWeb.WorkspaceLive.Components.DataFlowComponent do
   use ValentineWeb, :live_component
+  use PrimerLive
 
   def render(assigns) do
     ~H"""
     <div>
-      <div class="BtnGroup mb-2">
-        <button type="button" class="btn BtnGroup-item" phx-click="add_node" phx-value-type="actor">
-          Add Actor
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="add_node" phx-value-type="process">
-          Add Process
-        </button>
-        <button
-          type="button"
-          class="btn BtnGroup-item"
-          phx-click="add_node"
-          phx-value-type="datastore"
-        >
-          Add Datastore
-        </button>
-
-        <button type="button" class="btn BtnGroup-item" phx-click="fit_view">
-          Fit View
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="group_nodes">
-          Add boundary
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="merge_group">
-          Extend boundary
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="remove_group">
-          Remove boundary
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="clear_dfd">
-          Clear diagram
-        </button>
-        <button type="button" class="btn BtnGroup-item" phx-click="remove_elements">
-          Remove elements
-        </button>
+      <div class="d-flex mb-2">
+        <.button_group class="mr-2">
+          <.button is_icon_button aria-label="Add Actor" phx-click="add_node" phx-value-type="actor">
+            <.octicon name="person-24" />
+          </.button>
+          <.button
+            is_icon_button
+            aria-label="Add Process"
+            phx-click="add_node"
+            phx-value-type="process"
+          >
+            <.octicon name="circle-24" />
+          </.button>
+          <.button
+            is_icon_button
+            aria-label="Add Datastore"
+            phx-click="add_node"
+            phx-value-type="datastore"
+          >
+            <.octicon name="stack-24" />
+          </.button>
+        </.button_group>
+        <.button_group class="mr-2">
+          <.button is_icon_button aria-label="Add Boundary" phx-click="group_nodes">
+            <.octicon name="fold-up-24" />
+          </.button>
+          <.button is_icon_button aria-label="Extend Boundary" phx-click="merge_group">
+            <.octicon name="unfold-24" />
+          </.button>
+          <.button is_icon_button aria-label="Remove Boundary" phx-click="remove_group">
+            <.octicon name="fold-24" />
+          </.button>
+        </.button_group>
+        <.button_group class="mr-2">
+          <.button is_icon_button aria-label="Fit View" phx-click="fit_view">
+            <.octicon name="project-template-24" />
+          </.button>
+        </.button_group>
+        <.button_group class="mr-2">
+          <.button is_icon_button aria-label="Remove elements" phx-click="remove_elements">
+            <.octicon name="x-24" />
+          </.button>
+          <.button is_icon_button aria-label="Clear diagram" phx-click="clear_dfd">
+            <.octicon name="trash-24" />
+          </.button>
+        </.button_group>
       </div>
-      <div class="Box color-bg-default">
+      <.box>
         <div id="cytoscape-container" class="Box-body">
           <div id="cy" tabindex="0"></div>
         </div>
-      </div>
+      </.box>
     </div>
     """
   end
