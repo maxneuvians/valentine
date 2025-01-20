de23e382f5b79f9b6764860e6ef33ea09d5065b0
Max Neuvians <max@neuvians.net>
2025-01-20 20:42:02 +0000

    fix: gmail email vs ids

--- valentine/lib/valentine_web/controllers/auth_controller.ex
+++ valentine/lib/valentine_web/controllers/auth_controller.ex
@@ -11,8 +11,8 @@ defmodule ValentineWeb.AuthController do
   def callback(%{assigns: %{ueberauth_auth: %Ueberauth.Auth{} = auth}} = conn, _params) do
     conn
     |> clear_session()
-    |> put_session(:user_id, auth.uid)
-    |> put_session(:live_socket_id, "users_socket:#{auth.uid}")
+    |> put_session(:user_id, auth.info.email)
+    |> put_session(:live_socket_id, "users_socket:#{auth.info.email}")
     |> redirect(to: ~p"/workspaces")
   end
 end
